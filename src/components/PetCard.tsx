3bb70de8061b8729a4a89eb4ea691bfc18960750
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2024-05-14 21:03:38 +0200

    Add species to card

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { mockPets } from "@/data/data";
-import { Button } from "@/components/ui/button";
+import React from 'react';
+import { mockPets } from '@/data/data';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -8,23 +8,25 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
+} from '@/components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Link from 'next/link';
 
 interface PetCardProps {
   name: string;
   dateOfBirth: string;
+  species: string;
 }
-export const PetCard = ({ name, dateOfBirth }: PetCardProps) => {
+export const PetCard = ({ name, dateOfBirth, species }: PetCardProps) => {
   return (
-    <Card className="w-[350px]">
+    <Card className='w-[350px]'>
       <CardHeader>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         <CardTitle>{name}</CardTitle>
+        <p>{species}</p>
         <CardDescription>Born on {dateOfBirth}</CardDescription>
       </CardHeader>
       <CardContent>
