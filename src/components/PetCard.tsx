aaa3255c40b2c6efb90376d4622eaaecad011de1
Jagoda Kubicka <jag.kubicka@gmail.com>
2024-06-06 17:18:02 +0200

    test

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { mockPets } from "@/data/data";
-import { Button } from "@/components/ui/button";
+import React from 'react';
+import { mockPets } from '@/data/data';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -8,9 +8,9 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
+} from '@/components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Link from 'next/link';
 
 interface PetCardProps {
   name: string;
@@ -18,10 +18,10 @@ interface PetCardProps {
 }
 export const PetCard = ({ name, dateOfBirth }: PetCardProps) => {
   return (
-    <Card className="w-[350px]">
+    <Card className='w-[350px]'>
       <CardHeader>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         <CardTitle>{name}</CardTitle>
@@ -30,6 +30,7 @@ export const PetCard = ({ name, dateOfBirth }: PetCardProps) => {
       <CardContent>
         <Link href={`/pets/${name}`}>
           <Button>See more</Button>
+          <p>test</p>
         </Link>
       </CardContent>
     </Card>
