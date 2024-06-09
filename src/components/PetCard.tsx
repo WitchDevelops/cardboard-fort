651f5fa2b58e21accf151a5cd2531680885e5b81
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-06-09 12:43:29 +0200

    #5 install react-icons library and check if it works

--- src/components/PetCard.tsx
+++ src/components/PetCard.tsx
@@ -11,28 +11,50 @@ import {
 } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import Link from 'next/link';
+import { TbGenderMale, TbGenderFemale } from 'react-icons/tb';
 
 interface PetCardProps {
   name: string;
   dateOfBirth: string;
   species: string;
+  gender?: string;
 }
-export const PetCard = ({ name, dateOfBirth, species }: PetCardProps) => {
+export const PetCard = ({
+  name,
+  dateOfBirth,
+  species,
+  gender,
+}: PetCardProps) => {
+  const genderDescription =
+    gender === 'male' ? (
+      <>
+        <TbGenderMale />
+        <span>MALE</span>
+      </>
+    ) : gender === 'female' ? (
+      <>
+        <TbGenderFemale />
+        <span>FEMALE</span>
+      </>
+    ) : null;
+
   return (
-    <Card className='w-[350px]'>
+    <Card className="w-[350px]">
       <CardHeader>
         <Avatar>
-          <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         <CardTitle>{name}</CardTitle>
         <p>{species}</p>
         <CardDescription>Born on {dateOfBirth}</CardDescription>
+        {genderDescription && (
+          <CardDescription className="flex items-center gap-1">{genderDescription}</CardDescription>
+        )}
       </CardHeader>
       <CardContent>
         <Link href={`/pets/${name}`}>
           <Button>See more</Button>
-          <p>test</p>
         </Link>
       </CardContent>
     </Card>
