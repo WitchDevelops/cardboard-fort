cb20627d2a3edbd3a58c74b3794965d09a353abf
Jagoda Kubicka <jag.kubicka@gmail.com>
2024-06-14 21:09:02 +0200

    4-tailwind custom classes file and tailwind variables reset

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,19 +5,17 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+          'bg-transparent border-2 border-primary rounded-xl text-black shadow hover:bg-primary/90',
         outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+          'bg-secondary text-secondary shadow-sm hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
