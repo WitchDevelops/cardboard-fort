efcc12b1ca3ab511569bd8d8f1128f06ed607640
Jagoda Kubicka <jag.kubicka@gmail.com>
2024-06-18 14:19:18 +0200

    #10 file naming convention

--- src/components/img/PetCardImage.tsx
+++ src/components/img/PetCardImage.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { UnavailableImg } from './UnavailableImg';
+import { UnavailableImage } from './UnavailableImage';
 
 interface PetCardImageProps {
   img?: string;
@@ -9,7 +9,7 @@ interface PetCardImageProps {
 export const PetCardImage: React.FC<PetCardImageProps> = ({ img, alt }) => {
   return (
     <div className="absolute top-0 left-0 rounded-xl w-full h-full shrink-0 overflow-hidden z-0">
-      <Suspense fallback={<UnavailableImg />}>
+      <Suspense fallback={<UnavailableImage />}>
         {img ? (
           <img
             className="aspect-square h-full w-full object-cover"
@@ -17,7 +17,7 @@ export const PetCardImage: React.FC<PetCardImageProps> = ({ img, alt }) => {
             alt={alt}
           />
         ) : (
-          <UnavailableImg />
+          <UnavailableImage />
         )}
       </Suspense>
     </div>
--- src/components/img/UnavailableImage.tsx
+++ src/components/img/UnavailableImage.tsx
@@ -1,6 +1,6 @@
 import { TbPaw } from 'react-icons/tb';
 
-export const UnavailableImg: React.FC = () => {
+export const UnavailableImage: React.FC = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <TbPaw className="w-full h-full" />
