2f658afbb4ba0758c765ba01a9131c09921753d9
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-06-26 17:27:41 +0200

    style pet tabs (WIP)

--- src/components/BasicInfo.tsx
+++ src/components/BasicInfo.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { EditablePetInfo } from './EditablePetInfo';
-interface PetInfoProps {
-  pet: {
-    img?: string;
-  };
-}
+import { EditablePetInfo } from '@/components/EditablePetInfo';
+import { Tag } from '@/components/Tag';
+import Image from 'next/image';
 
+// TODO: type the interface
 export const BasicInfo = ({ pet }: any) => {
   return (
-    <div>
-      <div className="flex">
-        <div className="rounded-xl w-[25%] overflow-hidden col-span-1">
-          <img className="aspect-auto h-full w-full " src={pet.img} />
-        </div>
-        <div className="w-[75%]">
-          <EditablePetInfo pet={pet} />
-        </div>
+    <div
+      className="sm:grid grid-cols-2 gap-4"
+      style={{ gridTemplateColumns: '1fr 1fr 1fr' }}
+    >
+      <div
+        className="rounded-xl shadow-md aspect-auto overflow-hidden"
+        style={{ gridColumn: '1/2' }}
+      >
+        <img className="object-cover" src={pet.img} />
+        {/* TODO: use <Image> */}
+        {/* problem: unconfigured host, look into that */}
+        {/* <Image src={pet.img} alt={pet.name} width='100' height='200' /> */}
       </div>
-      <div className="flex">
-        <div className="w-[75%]">
-          <p>Description</p>
-          <p>{pet.bio}</p>
-        </div>
-        <div className="w-[25%]">
-          <p>Tags</p>
-          <div>
-            <p>Cute</p>
-            <p>Energetic</p>
+      <div className="rounded-xl shadow-md p-4" style={{ gridColumn: '2/4' }}>
+        <EditablePetInfo pet={pet} />
+      </div>
+      <div className="rounded-xl shadow-md p-4" style={{ gridColumn: '1/3' }}>
+        <p className="font-bold text-md p-2">Description</p>
+        <p className="bg-slate-50 rounded-lg px-4 py-2">{pet.bio}</p>
+      </div>
+      <div className="rounded-xl shadow-md p-4">
+        <p className="font-bold text-md p-2">Tags</p>
+        <div className="bg-slate-50 rounded-md p-2">
+          <div className="flex gap-2">
+            <Tag tagText="cute" />
+            <Tag tagText="energetic" />
           </div>
         </div>
       </div>
--- src/components/PetTabs.tsx
+++ src/components/PetTabs.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { formatDate } from '@/utils/date/formatDate';
-import { calculateAge } from '@/utils/date/calculateAge';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { BasicInfo } from '@/components/BasicInfo';
 
@@ -16,14 +14,10 @@ interface PetTabsProps {
 }
 
 export const PetTabs = (props: PetTabsProps) => {
-  const currentDate = new Date();
-  const formattedDateOfBirth = formatDate(props.date_of_birth);
-  const age = calculateAge(props.date_of_birth, currentDate);
-  console.log(props);
   return (
     <div>
       <Tabs defaultValue="basicInfo" className="w-[80vw] mx-auto">
-        <TabsList className="flex justify-between">
+        <TabsList>
           <TabsTrigger value="basicInfo">Basic Info</TabsTrigger>
           <TabsTrigger value="vaccinations">Vaccinations</TabsTrigger>
           <TabsTrigger value="medications">Medications</TabsTrigger>
