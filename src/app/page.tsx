20c9de68d7b270be6235282abed3c0b23d5d89b1
Jagoda Kubicka <jag.kubicka@gmail.com>
2024-07-01 17:51:51 +0200

    #35 style welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { PetGrid } from '@/components/PetGrid';
-import { AddPetModal } from '@/components/modals/AddPetModal';
-import { Toaster } from '@/components/ui/toaster';
 import { font_accent } from '@/components/ui/fonts';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
@@ -8,16 +5,46 @@ import '@/app/styles/main.scss';
 
 export default function Home() {
   return (
-    <div className="grid place-items-center h-[100vh] text-center doors-of-durin">
+    <div className="h-[100vh] w-full main">
       <main className="text-center">
-        <h1 className={`${font_accent.className} font-semibold text-3xl`}>
-          Pet app home page
-        </h1>
-        <div>
-          <p>Speak, friend, and</p>
-          <Link href="user">
-            <Button>Enter</Button>
-          </Link>
+        <div className="main-img main-content">
+          <div className="main-img__blob main-img__blob--first"></div>
+
+          <div className="main-img__blob main-img__blob--second"></div>
+
+          <div className="main-img__blob main-img__blob--third"></div>
+
+          <div className="main-img__blob main-img__blob--fourth"></div>
+        </div>
+
+        <div className="main-text main-content">
+          <h1
+            className={`${font_accent.className} font-semibold text-4xl text-white-100`}
+          >
+            Welcome to Pet App
+          </h1>
+          <h2 className="text-xl text-white-100 text-balance">
+            Keep Your Pet's Health in Check, One Paw at a Time!
+          </h2>
+          <h3 className="text-xl text-white-100 text-balance">
+            Track their medical history, manage appointments, and ensure a
+            healthy, happy life for your furry friend.
+          </h3>
+          <div className="flex gap-4">
+            <Link href="user">
+              <Button
+                variant={'outline'}
+                className="text-white-100 hover:text-primary hover:bg-white-100"
+              >
+                Login
+              </Button>
+            </Link>
+            <Link href="user">
+              <Button className="bg-white-100 text-primary hover:bg-transparent hover:text-white-100 border hover:border-white-100">
+                Signup
+              </Button>
+            </Link>
+          </div>
         </div>
       </main>
     </div>
