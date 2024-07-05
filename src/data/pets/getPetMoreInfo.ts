c1928852b39b34037370beaf0c6a0e8904d39ca3
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-07-05 10:10:33 +0200

    #39 del console.log, refactor to try/catch

--- src/data/pets/getPetMoreInfo.ts
+++ src/data/pets/getPetMoreInfo.ts
@@ -1,10 +1,16 @@
 import { supabase } from '@/services/supabase';
 
 export const getPetMoreInfo = async () => {
-  const { data: pets, error } = await supabase.from('pets_more_info').select('*');
-  if (error) {
-    console.log(error);
+  try {
+    const { data: pets, error } = await supabase
+      .from('pets_more_info')
+      .select('*');
+    if (error) {
+      throw error;
+    }
+    return pets;
+  } catch (error) {
+    console.error(error);
     return null;
   }
-  return pets;
 };
--- src/pages/user/pets/[pet_id].js
+++ src/pages/user/pets/[pet_id].js
@@ -11,49 +11,56 @@ import { PetTabs } from '@/components/PetTabs';
 import { ButtonsOnPetPage } from '@/components/ButtonsOnPetPage';
 
 export async function getStaticPaths() {
-  const { data: pets, error } = await supabase
-    .from('pets_data')
-    .select('pet_id');
+  try {
+    const { data: pets, error } = await supabase
+      .from('pets_data')
+      .select('pet_id');
 
-  if (error) {
+    if (error) {
+      throw error;
+    }
+
+    const paths = pets.map((pet) => ({
+      params: { pet_id: pet.pet_id },
+    }));
+
+    return { paths, fallback: false };
+  } catch (error) {
     console.error(error);
     return { paths: [], fallback: false };
   }
-
-  const paths = pets.map((pet) => ({
-    params: { pet_id: pet.pet_id },
-  }));
-
-  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const { data: pet, error: petError } = await supabase
-    .from('pets_data')
-    .select(
+  try {
+    const { data: pet, error: petError } = await supabase
+      .from('pets_data')
+      .select(
+        `
+        *,
+        pets_more_info (
+          microchip_nr,
+          pet_bio
+        )
       `
-      *,
-      pets_more_info (
-        microchip_nr,
-        pet_bio
       )
-    `
-    )
-    .eq('pet_id', params.pet_id)
-    .single();
+      .eq('pet_id', params.pet_id)
+      .single();
 
-  if (petError) {
-    console.error(petError);
+    if (petError) {
+      throw petError;
+    }
+
+    return {
+      props: { pet },
+    };
+  } catch (error) {
+    console.error(error);
     return { notFound: true };
   }
-
-  return {
-    props: { pet },
-  };
 }
 
 export default function PetPage({ pet }) {
-  console.log(pet);
   return (
     <div className="w-[80vw] max-w-[900px] m-auto">
       <div>
