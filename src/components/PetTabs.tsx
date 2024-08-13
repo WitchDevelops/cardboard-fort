9cab41deb7c835525c603bdbba23b6fea7e372fc
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-08-13 10:46:25 +0200

    #60 remove Suspense from PetTabs component

--- src/components/PetTabs.tsx
+++ src/components/PetTabs.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { BasicInfo } from '@/components/BasicInfo';
 import { PetData } from '@/utils/types/petData';
 import { Vaccinations } from '@/components/Vaccinations';
 import { Medications } from '@/components/Medications';
-import { Loader } from '@/components/Loader';
 
 export const PetTabs: React.FC<PetData> = ({ ...pet }) => {
   return (
@@ -17,19 +16,13 @@ export const PetTabs: React.FC<PetData> = ({ ...pet }) => {
           <TabsTrigger value="appointments">Appointments</TabsTrigger>
         </TabsList>
         <TabsContent value="basicInfo">
-          <Suspense fallback={<Loader />}>
-            <BasicInfo {...pet} />
-          </Suspense>
+          <BasicInfo {...pet} />
         </TabsContent>
         <TabsContent value="vaccinations">
-          <Suspense fallback={<Loader />}>
-            <Vaccinations pet_id={pet.pet_id} />
-          </Suspense>
+          <Vaccinations pet_id={pet.pet_id} />
         </TabsContent>
         <TabsContent value="medications">
-          <Suspense fallback={<Loader />}>
-            <Medications pet_id={pet.pet_id} />
-          </Suspense>
+          <Medications pet_id={pet.pet_id} />
         </TabsContent>
         <TabsContent value="appointments">Appointments</TabsContent>
       </Tabs>
