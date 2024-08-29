1ba29df1fa106d7352548f66d8a73fd3743b48de
Jagoda Kubicka <jag.kubicka@gmail.com>
2024-08-29 21:10:15 +0200

    #64 styling different layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button';
 import { font_accent } from '@/components/ui/fonts';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div className="gradient-bg flex flex-col justify-center items-center min-h-screen">
-      <div className="flex flex-col justify-center items-center gap-4 min-h-screen bg-[url('/pet-img/404.png')] bg-bottom bg-no-repeat p-5">
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <Image src={'/pet-img/404.png'} width={400} height={400} alt={''} />
+      <div className="flex flex-col justify-center items-center gap-4 gradient-bg p-5 rounded-2xl">
         <h2
           className={`${font_accent.className} font-bold text-white text-6xl sm:text-9xl`}
         >
